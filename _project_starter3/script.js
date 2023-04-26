cdfb0946b3b8da8bc70412fde1cccd9bf62cfc45
VictorRait <115611038+VictorRait@users.noreply.github.com>
2023-04-26 09:34:31 +0800

    Project3 Rotating Nav

---
+++ _project_starter3/script.js
@@ -0,0 +1,10 @@
+const close = document.querySelector("#close");
+const open = document.querySelector("#open");
+const container = document.querySelector(".container");
+
+open.addEventListener("click", function () {
+  container.classList.add("show-nav");
+});
+close.addEventListener("click", function () {
+  container.classList.remove("show-nav");
+});
